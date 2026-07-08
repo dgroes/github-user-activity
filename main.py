e78d76a6fa7e8cdd7cc4df72c3c4b29808a18fc1
Diego Pasten <diegopasten78@gmail.com>
2026-07-08 11:55:37 -0400

    feat: :building_construction: Se implementa la vista de todos los eventos del usuario

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 import urllib.request
-import sys
 import json
 from collections import Counter
-
-def main():
-    # sys.argv[0] es siempre el nombre del archivo ("main.py")
-    # Validamos que se hayan pasado suficientes argumentos
-    if len(sys.argv) < 1:
-        print("[GITHUB-ACTIVITY]: Para buscar la actividad de un usuario introdusca un username. Uso: python main.py <username>")
-        return
-
-    elif len(sys.argv) > 2:
-        print("[ERROR]: El programa solo recibe un parametro. Uso: python main.py <username>")
-        return
-    username = sys.argv[1] # Captura del username
-
-    return username
+from cli import main
 
 
 username = main()
@@ -25,25 +11,29 @@ url = f'https://api.github.com/users/{username}/events'
 # Abre una URL y lee la respuesta
 with urllib.request.urlopen(url, data=None) as response:
     html = response.read()
-    
     dato = json.loads(html)
-    lista = []
-    for diccionario in dato:
-        if diccionario.get("type") == "PushEvent":
-                #print("---- Evento Push Encontrado ----")
-                name = diccionario.get("repo", {}).get("name")
-                lista.append(name)
-
-               
-                #print(json.dumps(diccionario, indent=4, ensure_ascii=False))
-
     
-    # print(diccionario.get("repo"))
-    contador = Counter(lista)
-    for repo, cantidad in contador.items():
-         print(f" - {cantidad} Push Events hacia el repositorio {repo}")
+    combinaciones = []
+    
+    for diccionario in dato:
+        tipo = diccionario.get("type")
+        repo = diccionario.get("repo", {}).get("name")
+        
+        combinaciones.append((tipo, repo))
+        # Quedaría así: [('PushEvent', 'dgroes/github-user-activity'), ('PushEvent', 'dgroes/github-user-activity')...
 
-    with open('salida.json', 'w', encoding='utf-8') as archivo:
+    #Conteo final
+    conteo_final = Counter(combinaciones)
+    #print(combinaciones)
+
+    # Impresión limpia
+    print(f"\n📊 Actividad de {username}:")
+    for (tipo, repo), cantidad in conteo_final.items():
+        print(f"- {cantidad} {tipo} hacia el repositorio {repo}")
+
+
+    # Guardar la última consulta "total" dentro de un fichero
+    with open('output.json', 'w', encoding='utf-8') as archivo:
         json.dump(dato, archivo, indent=4)
 
     
